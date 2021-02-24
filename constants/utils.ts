fcc7395bb78dcda570e526038e950d0d19f4c6af
Erko Bridee <erko.bridee@gmail.com>
2021-02-24 10:49:04 +0100

    refactor: move project code into the src folder

--- constants/utils.ts
+++
@@ -1,3 +0,0 @@
-export const TO_STRING = {}.toString;
-
-export const NOOP = () => undefined;
---
+++ src/constants/utils.ts
@@ -0,0 +1,5 @@
+import { TFunction } from '../types/functions';
+
+export const TO_STRING = {}.toString;
+
+export const NOOP: TFunction = () => undefined;
