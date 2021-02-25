8e399ba400ef9d28d18962a2342c92a7720c12e8
Erko Bridee <erko.bridee@gmail.com>
2021-02-25 10:31:06 +0100

    feat(types): utils

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,2 +1,8 @@
 /** a typescript helper to easy get which is the type of a Object property */
 export type TPropType<TObj, TProp extends keyof TObj> = TObj[TProp];
+
+/** JavaScript Object type */
+export type TJSObject<T = unknown> = Record<string, T> & Object;
+
+/** JavaScript value that could be an object or any other js value */
+export type TJSValue<T = unknown> = TJSObject<T> | unknown;
