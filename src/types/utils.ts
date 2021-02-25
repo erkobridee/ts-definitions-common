d7d9cf3f25197a3d86a0839cf8b959b16ab3e979
Erko Bridee <erko.bridee@gmail.com>
2021-02-25 10:34:55 +0100

    fix(types): utils

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -2,7 +2,7 @@
 export type TPropType<TObj, TProp extends keyof TObj> = TObj[TProp];
 
 /** JavaScript Object type */
-export type TJSObject<T = unknown> = Record<string, T> & Object;
+export type TJSObject<T = unknown> = Record<string, T>;
 
 /** JavaScript value that could be an object or any other js value */
 export type TJSValue<T = unknown> = TJSObject<T> | unknown;
